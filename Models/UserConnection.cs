01aafee88a2c106d1890b141a194900a277e02b6
Hamza Susam <hamzassm18@gmail.com>
2025-02-23 20:11:59 +0300

    feat: solution => project and solution names changed

---
+++ Models/UserConnection.cs
@@ -0,0 +1,7 @@
+namespace SignalRWithNet8.SignalRWithNet8.ChatService.Models;
+
+public class UserConnection
+{
+    public string UserName { get; set; }
+    public string ChatRoom { get; set; }
+}
