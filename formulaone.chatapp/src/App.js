ea2480cc7ec4891207c1d192f18c64974282249b
Hamza Susam <hamzassm18@gmail.com>
2025-02-27 19:17:28 +0300

    feat: chatapp => app.js, joinChatRoom method added

--- formulaone.chatapp/src/App.js
+++ formulaone.chatapp/src/App.js
@@ -2,8 +2,32 @@ import {Col, Row, Container} from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WaitingRoom from './components/waitingRoom';
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 function App() {
+  const[conn, setConnection] = useState();
+
+  const joinChatRoom = async (username, chatroom) => {
+    try {
+      
+      const conn = new HubConnectionBuilder()
+        .withUrl('http://localhost:5125')
+        .configureLogging(LogLevel.Information)
+        .build();
+
+        conn.on("JoinSpecificChatRoom", (username, msg) => {
+          console.log(`${username} says ${msg}`);
+        });
+
+        await conn.start();
+        await conn.invoke("JoinSpecificChatRoom", {username, chatroom});
+
+        setConnection(conn);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
       <main>
@@ -13,7 +37,7 @@ function App() {
               <h1 className='font-weight-light'>Formula One Chat App</h1>
             </Col>
           </Row>
-          <WaitingRoom>
+          <WaitingRoom joinChatRoom={joinChatRoom}>
             
           </WaitingRoom>
         </Container>
